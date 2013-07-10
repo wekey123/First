88aed4b59d1098122b61537099cc7fd054d11e95
Wekey <mail2rmvignesh@gmail.com>
2013-07-10 19:03:57 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Created on Jul 10, 2013
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ echo "hello";	
+?>
